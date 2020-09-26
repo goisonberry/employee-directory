6eb136a90c9c3dec4667da80db7eee056b05cd6a
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-09-26 13:45:05 -0700

    Updates

---
+++ precache-manifest.d0400e6839398e323297766063a31a17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382fbd0c8f4a07928f025c2abb67f932",
+    "url": "/goisonberry/employee-directory/index.html"
+  },
+  {
+    "revision": "87a518e348471fc6224a",
+    "url": "/goisonberry/employee-directory/static/css/main.1a0bccf2.chunk.css"
+  },
+  {
+    "revision": "010866e6ce1858769151",
+    "url": "/goisonberry/employee-directory/static/js/2.c7f7d248.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goisonberry/employee-directory/static/js/2.c7f7d248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87a518e348471fc6224a",
+    "url": "/goisonberry/employee-directory/static/js/main.d66789d6.chunk.js"
+  },
+  {
+    "revision": "92d753de34a1df7ca64c",
+    "url": "/goisonberry/employee-directory/static/js/runtime-main.2c42741d.js"
+  }
+]);
